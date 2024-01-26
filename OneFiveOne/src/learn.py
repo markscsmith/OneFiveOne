7b0e524d8f172fe72ead24e6b093fea4d9913c31
mscs <markscsmith@gmail.com>
2024-01-26 01:00:46 -0500

    updates to model for larger model size

--- OneFiveOne/src/learn.py
+++ OneFiveOne/src/learn.py
@@ -10,20 +10,24 @@ from timg import Renderer, Ansi24HblockMethod
 from PIL import Image
 import io
 import base64
-
+import time
+from stable_baselines3.common.env_util import SubprocVecEnv
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.policies import ActorCriticPolicy
 import sys
 
-
 class CustomFeatureExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.Space, features_dim: int = 128):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
+        
         # Define your custom layers here
         self.extractor = nn.Sequential(
-            nn.Linear(np.prod(observation_space.shape), features_dim),
+            nn.Linear(np.prod(observation_space.shape), 256),
             nn.ReLU(),
-            # You can add more layers here
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, features_dim),
+            nn.ReLU(),
         )
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
@@ -35,11 +39,12 @@ class CustomNetwork(ActorCriticPolicy):
         super(CustomNetwork, self).__init__(
             observation_space,
             action_space,
-            lr_schedule=lambda _: 0.002,  # Replace with your learning rate schedule
+            # lr_schedule=lambda _: 0.002,  # Replace with your learning rate schedule
+            lr_schedule=lambda _: 0.025, 
             features_extractor_class=CustomFeatureExtractor,
             features_extractor_kwargs={},
-            net_arch=[128, 128],  # Define the architecture of the network
-            activation_fn=nn.ReLU
+            net_arch=[dict(pi=[128, 64, 32], vf=[128, 64, 32])],  # Define separate architectures for policy and value networks
+            activation_fn=nn.ReLU,
         )
 
 
@@ -47,7 +52,7 @@ class CustomNetwork(ActorCriticPolicy):
 class PyBoyEnv(gym.Env):
     def __init__(self, game_path, emunum):
         super(PyBoyEnv, self).__init__()
-        self.pyboy = PyBoy(game_path, window_type='headless')
+        self.pyboy = PyBoy(game_path, window_type="headless")
 
         # Define the memory range for 'number of Pokémon caught'
         self.caught_pokemon_start = 0xD2F7
@@ -60,22 +65,32 @@ class PyBoyEnv(gym.Env):
         # Here we define the observation space to include the game's entire memory
         # and an additional value for the 'number of Pokémon caught'
         # self.observation_space = gym.spaces.Box(low=0, high=255, shape=(0x10000 + 1,), dtype=np.uint8)
-        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(698,), dtype=np.uint8)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(698,), dtype=np.uint8
+        )
 
     def step(self, action, ticks=1):
+        
         # Apply an action to the emulator
 
         self.pyboy.send_input(action)
         for i in range(ticks):
             self.pyboy.tick()
 
-        if (self.emunum == 0 and self.pyboy.frame_count % 1000 == 0) or np.random.randint(777) == 0:
-            obj = Renderer()
-            obj.load_image(Image.fromarray(self.pyboy.botsupport_manager().screen().screen_ndarray()))
-            terminal_size = os.get_terminal_size()
-            if terminal_size.columns < 160 or terminal_size.lines < 144 / 2:
-                obj.resize(terminal_size.columns, terminal_size.lines * 2 * 160 // 144)
-            obj.render(Ansi24HblockMethod)
+        if (
+            self.emunum == 0 and self.pyboy.frame_count % 1000 == 0
+        ) or np.random.randint(77) == 0:
+            # obj = Renderer()
+            # obj.load_image(
+            #     Image.fromarray(
+            #         self.pyboy.botsupport_manager().screen().screen_ndarray()
+            #     )
+            # )
+            # terminal_size = os.get_terminal_size()
+            # if terminal_size.columns < 160 or terminal_size.lines < 144 / 2:
+            #     obj.resize(terminal_size.columns,
+            #                terminal_size.lines * 2 * 160 // 144)
+            # obj.render(Ansi24HblockMethod)
             print("OS:FRAME:", self.pyboy.frame_count)
             print("OS:EMUNUM:", self.emunum)
 
@@ -87,7 +102,9 @@ class PyBoyEnv(gym.Env):
         flo.seek(0)
         memory_values = np.frombuffer(flo.read(), dtype=np.uint8)
         # pokemon_caught = sum([self.pyboy.get_memory_value(address) for address in range(self.caught_pokemon_start, self.caught_pokemon_end + 1)])
-        pokemon_caught = sum(memory_values[self.caught_pokemon_start:self.caught_pokemon_end])
+        pokemon_caught = sum(
+            memory_values[self.caught_pokemon_start: self.caught_pokemon_end]
+        )
 
         memory_values = memory_values[0xD5A6:0xD85F]
         observation = np.append(memory_values, pokemon_caught)
@@ -111,9 +128,14 @@ class PyBoyEnv(gym.Env):
         self.pyboy.save_state(flo)
         flo.seek(0)
         memory_values = np.frombuffer(flo.read(), dtype=np.uint8)
-        pokemon_caught = sum(memory_values[self.caught_pokemon_start:self.caught_pokemon_end])
+        pokemon_caught = sum(
+            memory_values[self.caught_pokemon_start: self.caught_pokemon_end]
+        )
         unique_events = memory_values[0xD5A6:0xD85F]
-        hashable_strings = "".join([base64.b64encode(bytes(chunk)).decode('utf-8') for chunk in unique_events])
+        hashable_strings = "".join(
+            [base64.b64encode(bytes(chunk)).decode("utf-8")
+             for chunk in unique_events]
+        )
 
         if hashable_strings not in self.seen_events:
             self.seen_events.add(hashable_strings)
@@ -121,17 +143,21 @@ class PyBoyEnv(gym.Env):
 
         memory_values = memory_values[0xD5A6:0xD85F]
         seed = 0
-        observation = np.append(memory_values, pokemon_caught + len(self.seen_events))
+        observation = np.append(
+            memory_values, pokemon_caught + len(self.seen_events))
         # use timg to output the current screen
 
-        
-        obj = Renderer()
-        
-        obj.load_image(Image.fromarray(self.pyboy.botsupport_manager().screen().screen_ndarray()))
-        terminal_size = os.get_terminal_size()
-        if terminal_size.columns < 160 or terminal_size.lines < 144 / 2:
-            obj.resize(terminal_size.columns, terminal_size.lines * 2 * 160 // 144)
-        obj.render(Ansi24HblockMethod)
+        # obj = Renderer()
+
+        # obj.load_image(
+        #     Image.fromarray(
+        #         self.pyboy.botsupport_manager().screen().screen_ndarray())
+        # )
+        # terminal_size = os.get_terminal_size()
+        # if terminal_size.columns < 160 or terminal_size.lines < 144 / 2:
+        #     obj.resize(terminal_size.columns,
+        #                terminal_size.lines * 2 * 160 // 144)
+        # obj.render(Ansi24HblockMethod)
         print("OS:SHAPE:", observation.shape, seed)
         return observation
 
@@ -142,40 +168,73 @@ class PyBoyEnv(gym.Env):
 def train_model(env, steps):
     # You can adjust the policy keyword arguments as needed
     policy_kwargs = dict(
-        net_arch=dict(pi=[128, 128, 128], vf=[128, 128, 128]),  # Define the architecture of the policy and value networks
-        activation_fn=torch.nn.ReLU  # Activation function
+        features_extractor_class=CustomFeatureExtractor,
+        features_extractor_kwargs={},
+        net_arch=[dict(pi=[128, 64, 32], vf=[128, 64, 32])],
+        activation_fn=nn.ReLU,
     )
     # Autodetect mps, cuda, rocm
-    device = 'cpu'
-    device = 'mps' if torch.backends.mps.is_available() and torch.backends.mps.is_built() else device
-    device = 'cuda' if torch.cuda.is_available() else device
+    device = "cpu"
+    device = (
+        "mps"
+        if torch.backends.mps.is_available() and torch.backends.mps.is_built()
+        else device
+    )
+    device = "cuda" if torch.cuda.is_available() else device
 
-    model = PPO('MlpPolicy', env, policy_kwargs=policy_kwargs, verbose=1, device=device)  # Use 'cpu' if GPU is not available
+    model = PPO(
+        policy=CustomNetwork, env=env, policy_kwargs=policy_kwargs, verbose=1, device=device
+    )  # Use 'cpu' if GPU is not available
     model.learn(total_timesteps=steps)
     return model
 
 
+def make_env(game_path, save_state_path, emunum=0, steps=0, result_queue=None):
+    def _init():
+        env = PyBoyEnv(game_path, emunum=emunum)
+        env.pyboy.load_state(open(save_state_path, "rb"))
+        env.pyboy.set_emulation_speed(0)
+        return env
+
+    return _init
+
+
 # Function to run the emulator with the Gym environment
 def run_emulator(game_path, save_state_path, emunum=0, steps=0, result_queue=None):
-    env = PyBoyEnv(game_path, emunum=emunum)
-    env.pyboy.load_state(open(save_state_path, 'rb'))
+    print("Building Emulator: OS:EMUNUM:", emunum)
+    env = make_env(game_path, save_state_path, emunum, steps, result_queue)()
+    env.pyboy.load_state(open(save_state_path, "rb"))
     env.pyboy.set_emulation_speed(0)
+    # time.sleep(emunum)
     model = train_model(env, steps)
 
     obs = env.reset()
     actions = [None] * steps  # Create a pre-sized list of length "steps"
     ticks = 1
     for i in range(steps):
+        print(f"Step: {i} {emunum} started.")
         action, _ = model.predict(obs)
         # print("OS:ACTION:", action)
 
         actions[i] = action.tolist()
         obs = env.step(action, ticks=ticks)[0]
-    print(f"{len(actions)} Actions in thread {emunum}:", "".join([str(x) for x in actions]))
+        print(f"Step: {i} {emunum} complete.")
+    print(
+        f"{len(actions)} Actions in thread {emunum}:",
+        "".join([str(x) for x in actions]),
+    )
     env.close()
 
-if __name__ == '__main__':
-    steps = 20000
+
+def run_emulator_mp(game_path, save_state_path, emunum, steps, result_queue, cores):
+    print("Building Emulator: OS:EMUNUM:", emunum)
+    env = SubprocVecEnv([make_env(game_path, save_state_path, emunum, steps, result_queue)] for emunum in range(cores))
+
+    make_env(game_path, save_state_path, emunum, steps, result_queue)()
+
+
+if __name__ == "__main__":
+    steps = 1600000
     if len(sys.argv) < 2:
         print("Please provide the path to the game as a command line argument.")
         sys.exit(1)
@@ -193,7 +252,10 @@ if __name__ == '__main__':
     processes = []
 
     for i in range(num_cores):
-        process = multiprocessing.Process(target=run_emulator, args=(game_path, save_state_path, i, steps, result_queue))
+        process = multiprocessing.Process(
+            target=run_emulator,
+            args=(game_path, save_state_path, i, steps, result_queue),
+        )
         processes.append(process)
         process.start()
 
