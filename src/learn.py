94d55c4e62ca7885f8a3094ed183447b9898c22f
mscs <markscsmith@gmail.com>
2024-01-29 22:04:07 -0500

    🔧

--- src/learn.py
+++ src/learn.py
@@ -63,7 +63,7 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
 
         # Define your custom layers here
         self.extractor = nn.Sequential(
-            nn.Linear(np.prod(observation_space.shape), 2048),
+            nn.Linear(np.prod(observation_space.shape), 256),
             nn.ReLU(),
             nn.Linear(2048, 256),
             nn.ReLU(),
