0e7397626e100960311c2d2f67538bbcec25b201
mscs <markscsmith@gmail.com>
2024-02-11 19:33:38 -0500

    adjust rewards again and increase stationary frames to 14400.

--- src/learn.py
+++ src/learn.py
@@ -468,8 +468,9 @@ class PyBoyEnv(gym.Env):
         observation = np.append(memory_values, (pokemon_caught + 1) * len(self.visited_xy))
 
         # Don't count the frames where the player is still in the starting menus. Pokemon caught gives more leeway on standing still
-        reward = (pokemon_caught + 1) + len(self.visited_xy) + len(self.screen_image_arrays) // 1000 - (self.stationary_frames // 10) 
-        if self.stationary_frames > 5000:
+                                                                                                                               # Minutes standing still * 10
+        reward = (pokemon_caught) * 10000 + round(len(self.visited_xy) * (100 * len(self.screen_image_arrays) / self.frames))  -  round((self.stationary_frames // 24 // 60)) * 10
+        if self.stationary_frames > 14400:
             self.reset()
         # if np.random.randint(777) == 0 or self.last_pokemon_count != pokemon_caught or self.last_score - reward > 100:
         #     self.render()
