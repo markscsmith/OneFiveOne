1398de7d9c85cde342ffe7a3867927e3723efba7
mscs <markscsmith@gmail.com>
2024-03-10 00:24:49 -0500

    adjustments to learning rate due to stalls playing SNES :)

--- src/learn.py
+++ src/learn.py
@@ -678,9 +678,9 @@ if __name__ == "__main__":
         else:
             n_steps = steps * num_cpu
 
-            run_model = PPO(policy="MlpPolicy", n_steps=n_steps, batch_size=n_steps * num_cpu,  n_epochs=13,
-                            gamma=0.998, gae_lambda=0.998, learning_rate=learning_rate_schedule, env=env,
-                            policy_kwargs=policy_kwargs, verbose=1, device=device, ent_coef=0.9, vf_coef=0.10,)
+            run_model = PPO(policy="CnnPolicy", n_steps=n_steps, batch_size=n_steps * num_cpu,  n_epochs=3,
+                            gamma=0.92, gae_lambda=0.92, learning_rate=0.05, env=env,
+                            policy_kwargs=policy_kwargs, verbose=1, device=device, ent_coef=0.9, vf_coef=0.99,)
         # model_merge_callback = EveryNTimesteps(n_steps=steps * num_cpu * 1024, callback=ModelMergeCallback(args.num_hosts))
         # TODO: Progress callback that collects data from each frame for stats
         
@@ -691,5 +691,5 @@ if __name__ == "__main__":
             run_model.learn(total_timesteps=num_steps, progress_bar=False, callback=callbacks)
         return run_model
 
-    model = train_model(env, runsteps, steps=256, episodes=13)
+    model = train_model(env, runsteps, steps=512, episodes=13)
     model.save(f"{file_name}.zip")
