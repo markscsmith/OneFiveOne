7bcf3c8ed9f7d6fafbb7044f45de21e67a0984eb
mscs <markscsmith@gmail.com>
2024-03-17 12:27:59 -0400

    less aggressive update to learning rate in PPO algorithm

--- src/learn.py
+++ src/learn.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
                 gamma=0.998,  # Increased to give more importance to future rewards, can help escape repetitive actions.
                 gae_lambda=0.998,  # Adjusted for a better balance between bias and variance in advantage estimation.
                 # learning_rate=learning_rate_schedule,  # Standard starting point for PPO, adjust based on performance.
-                learning_rate=0.0025,
+                learning_rate=0.0006,
                 env=env,
                 policy_kwargs=policy_kwargs,  # Ensure this aligns with the complexities of your environment.
                 verbose=1,
