e58e56d94ccf39e7497afecc0eba0849f704af7b
mscs <markscsmith@gmail.com>
2024-04-02 23:13:10 -0400

    Increase number of epochs for more stable learning

--- src/learn.py
+++ src/learn.py
@@ -658,7 +658,7 @@ def train_model(env, total_steps, steps, episode, file_name):
                         # Reduce batch size if it's too large but ensure a minimum size for stability.
                         batch_size=steps // 8,
                         # Adjusted for potentially more stable learning across batches.
-                        n_epochs=1,
+                        n_epochs=3,
                         # Increased to give more importance to future rewards, can help escape repetitive actions.
                         gamma=0.9998,
                         # Adjusted for a better balance between bias and variance in advantage estimation.
