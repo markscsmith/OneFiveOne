3383d2b1eaec7587ee30c8c3b1b27c1481082487
mscs <mark.s.c.smith@gmail.com>
2024-05-24 00:21:53 -0400

    debugging to better understand what memory values currently look like

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -459,6 +459,7 @@ class PyBoyEnv(gym.Env):
                     caught_pokemon_start:caught_pokemon_end
                 ]
             ]
+        print("CAUGHT DEX:", len(caught_pokedex), caught_pokedex)
         pokemon_caught = np.sum(
            caught_pokedex
         )
@@ -472,6 +473,7 @@ class PyBoyEnv(gym.Env):
         pokemon_seen = np.sum(
            seen_pokdex
         )
+        print("SEEN DEX:", len(seen_pokdex), seen_pokdex)
 
         last_poke = self.last_pokemon_count
         last_poke_seen = self.last_seen_pokemon_count
