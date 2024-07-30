67248e7feb21ab2b5d96384f298c60c9238ed16d
mscs <mark.s.c.smith@gmail.com>
2024-07-29 20:27:18 -0400

    add the learning rate wobble back in

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -865,7 +865,7 @@ def train_model(
         gamma=0.998,
         # Adjusted for a better balance between bias and variance in advantage estimation.
         # gae_lambda=0.998,
-        # learning_rate=learning_rate_schedule,  # Standard starting point for PPO, adjust based on performance.
+        learning_rate=learning_rate_schedule,  # Standard starting point for PPO, adjust based on performance.
         # learning_rate=0.0002,
         # learning_rate=0.0003,
         # learning_rate=0.005,
