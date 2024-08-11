969ee9b442b7315f77a054ccb61a08b33bb5dc94
mscs <mark.s.c.smith@gmail.com>
2024-08-11 05:45:24 -0400

    try a more aggressively small gae_lambda and fewer runs because longer runs still got stuck and had decreasing rewards 🤔

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -859,7 +859,7 @@ def train_model(
         # Increased to give more importance to future rewards, can help escape repetitive actions.
         gamma=0.995,
         # Adjusted for a better balance between bias and variance in advantage estimation.
-        gae_lambda=0.90,
+        gae_lambda=0.50,
         learning_rate=learning_rate_schedule,
         # Standard starting point for PPO, adjust based on performance.
         # learning_rate=0.0002,
@@ -981,7 +981,7 @@ if __name__ == "__main__":
     # max_frames = PRESS_FRAMES + RELEASE_FRAMES * runsteps
 
     # episodes = 13
-    episodes = 69
+    episodes = 5
 
     # batch_size = 512 // 4
     batch_size = 64
