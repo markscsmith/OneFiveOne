9c69d5d37ceaa3ae4be550051d55b331e00d9adb
mscs <mark.s.c.smith@gmail.com>
2024-08-13 00:09:13 -0400

    chore: Update gae_lambda to 0.99 for more stable learning and better balance in advantage estimation maybe?

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -856,7 +856,7 @@ def train_model(
         batch_size=batch_size,
         n_epochs=3,
         gamma=0.995,
-        gae_lambda=0.95,
+        gae_lambda=0.99,
         # learning_rate=learning_rate_schedule,
         env=env,
         policy_kwargs=policy_kwargs,
