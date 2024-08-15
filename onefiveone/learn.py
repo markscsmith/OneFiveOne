182dd7e1e6e953daca53fc01081b1b8bc7f41c51
mscs <mark.s.c.smith@gmail.com>
2024-08-15 02:48:47 -0400

    bigger batches

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -986,11 +986,11 @@ if __name__ == "__main__":
 
     # batch_size = 64
     # https://stackoverflow.com/questions/76076904/in-stable-baselines3-ppo-what-is-nsteps try using whole batch of n_steps as batch size?
-    batch_size = 256
+    batch_size = 2048
 
     # n_steps = 2048
 
-    n_steps = 256
+    n_steps = 2048
     # total_steps = n_steps * 1024 * 6
     # total_steps = (
     #     60 * 60 * (60 // (PRESS_FRAMES + RELEASE_FRAMES))
