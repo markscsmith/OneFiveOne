0c322a1c2c83a58cf72f249d7f5bcbb24bd9db31
mscs <mark.s.c.smith@gmail.com>
2024-08-27 03:28:27 -0400

    theoretical two weeks of play time, two days per episode / save file

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -1076,7 +1076,7 @@ if __name__ == "__main__":
     # max_frames = PRESS_FRAMES + RELEASE_FRAMES * runsteps
 
     # episodes = 13
-    episodes = 8
+    episodes = 7
 
     # batch_size = 512 // 4
 
@@ -1097,7 +1097,7 @@ if __name__ == "__main__":
     # total_steps = num_cpu * n_steps * 64
 
     # hours of play
-    hours = 1
+    hours = 48
     
     
     # each step is (PRESS_FRAMES + RELEASE_FRAMES) frames long, at 60fps.  
