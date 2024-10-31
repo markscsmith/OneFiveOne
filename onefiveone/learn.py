664cf6dd38cc39e2001339b51d1cf91943fdde29
Mark Smith <mark.s.c.smith@gmail.com>
2024-10-31 02:21:07 -0400

    increase batch_size to 32 and n_steps to 2048 for improved training efficiency

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -1183,11 +1183,11 @@ if __name__ == "__main__":
 
     # batch_size = 64
     # https://stackoverflow.com/questions/76076904/in-stable-baselines3-ppo-what-is-nsteps try using whole batch of n_steps as batch size?
-    batch_size = 16
+    batch_size = 32
 
     # n_steps = 2048
 
-    n_steps = 1024
+    n_steps = 2048
     # total_steps = n_steps * 1024 * 6
     # total_steps = (
     #     60 * 60 * (60 // (PRESS_FRAMES + RELEASE_FRAMES))
