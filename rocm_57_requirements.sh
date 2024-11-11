014d2d27b6afe62c026c020da94882ba756757db
Mark Smith <mark.s.c.smith@gmail.com>
2024-11-11 01:31:30 -0500

    remove outdated files from trying to get stuff working on ROCm and old versions of the emulator code

--- rocm_57_requirements.sh
+++
@@ -1,4 +0,0 @@
-# bug in parsing of driver names in tensorflow-rocm 2.14.0.600
-pip3 install http://ml-ci.amd.com:21096/job/tensorflow/job/release-rocmfork-r214-rocm-enhanced/job/release-build-whl/lastSuccessfulBuild/artifact/packages-3.10/tensorflow_rocm-2.14.0.600-cp310-cp310-manylinux2014_x86_64.whl 
-
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7
