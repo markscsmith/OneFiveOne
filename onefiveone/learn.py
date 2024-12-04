99ae98193f65b7efeaa2801cc3ab10de0282fff0
Mark Smith <mark.s.c.smith@gmail.com>
2024-12-04 11:53:10 -0500

    Reduce number of epochs to reduce excessive memory consumption. 32 cores = just a shade under 64gb of RAM on ubuntu 24.04 with epochs=1

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -213,7 +213,7 @@ def train_model(
         n_steps=n_steps,
         # Reduce batch size if it's too large but ensure a minimum size for stability.
         batch_size=batch_size,
-        n_epochs=3,
+        n_epochs=1,
         gamma=0.998,
         gae_lambda=0.98,
         # learning_rate=learning_rate_schedule,
