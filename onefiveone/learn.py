701a63e74c0193abfaea3542338f3e64737f8255
Mark Smith <mark.s.c.smith@gmail.com>
2025-02-01 16:07:34 -0500

    remove parameters from train_model function

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -236,10 +236,10 @@ def train_model(
         batch_size=batch_size,
         # n_epochs=3,
         gamma=0.99,  # Reduced from 0.998
-        gae_lambda=0.98,
+        # gae_lambda=0.98,
         # learning_rate=learning_rate_schedule,
         # learning_rate=learning_rate_decay_schedule,
-        ent_coef=0.02,
+        # ent_coef=0.02,
         env=env,
         policy_kwargs=policy_kwargs,
         verbose=0,
