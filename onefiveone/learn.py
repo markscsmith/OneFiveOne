cb61de6ae5b5a1fb616894a60a1cfb26cb97032e
Mark Smith <mark.s.c.smith@gmail.com>
2025-02-01 20:12:00 -0500

    kernel size debugging

--- onefiveone/learn.py
+++ onefiveone/learn.py
@@ -195,7 +195,7 @@ class CustomCombinedExtractor(BaseFeaturesExtractor):
         self.conv = nn.Sequential(
             nn.Conv2d(screen_shape[2], 32, kernel_size=1, stride=2),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2),
+            nn.Conv2d(32, 64, kernel_size=2, stride=2),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=2),
             nn.ReLU(),
